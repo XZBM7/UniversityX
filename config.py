0aa08525958e9391c538f5fadb248a007955bb66
xzbrhom7 <151582327+XZBM7@users.noreply.github.com>
2025-07-10 19:18:16 +0300

    x

--- config.py
+++ config.py
@@ -1,30 +1,24 @@
+import os
 from pymongo import MongoClient
 from werkzeug.security import generate_password_hash
 from bson import ObjectId
 
-client = MongoClient("mongodb://localhost:27017/")
+# قراءة رابط المونجو من متغير البيئة MONGO_URI
+mongo_uri = os.environ.get("MONGO_URI")
+if not mongo_uri:
+    raise Exception("MONGO_URI environment variable is not set!")
+
+# إنشاء اتصال MongoDB باستخدام المتغير
+client = MongoClient(mongo_uri)
 db = client["university_system"]
+
 users = db.users
 exam_rooms_collection = db.exam_rooms
 quiz_results_collection = db.quiz_results
 complaints_collection = db.complaints
 payments_collection = db.payments
 
-# admin_user = {
-#     "name": "Admin - Omar",
-#     "email": "Omar@a.com",
-#     "password": generate_password_hash("Omar1234"),
-#     "role": "admin"
-# }
-
-# existing_admin = users.find_one({"email": admin_user["email"]})
-
-# if not existing_admin:
-#     users.insert_one(admin_user)
-#     print("✅ Admin user added")
-# else:
-#     print("⚠️ Admin user already exists, skipping insertion")
-
+# تحديث بيانات الطلاب (مثال)
 db.users.update_many(
     {"role": "student", "payment_status": {"$exists": False}},
     {"$set": {"payment_status": "not paid"}}
@@ -37,9 +31,4 @@ subjects_collection.update_one(
     {'$set': {'doctor_id': ObjectId('6869561965e2d6f5133bba6f')}} 
 )
 
-
 users.delete_many({"email": "admin@example.com"})
-
-# users.insert_one(admin_user)
-
-# print("✅ Admin user added ")
